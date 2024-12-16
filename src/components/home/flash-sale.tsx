41ba8d0d6a9b39030273c9633630d8b8429479e2
md-ali-0 <mohammad.98482@gmail.com>
2024-12-17 03:23:38 +0600

    some issue fixed

--- src/components/home/flash-sale.tsx
+++ src/components/home/flash-sale.tsx
@@ -20,7 +20,7 @@ const FlashSale = async () => {
                         Show More
                     </Link>
                 </div>
-                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6">
                     {products?.filter(product => product.discount > 0).map((product: Product) => (
                         <FlashSaleProductCard key={product.id} product={product}/>
                     ))}
