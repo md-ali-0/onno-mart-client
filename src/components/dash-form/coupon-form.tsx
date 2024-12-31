12c748f9c26c0b0d661f9d713f0ef9b1f73769fe
md-ali-0 <mohammad.98482@gmail.com>
2024-12-31 23:57:27 +0600

    coupon form updated

--- src/components/dash-form/coupon-form.tsx
+++ src/components/dash-form/coupon-form.tsx
@@ -10,7 +10,7 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useCreateBrandMutation } from "@/redux/features/brand/brandApi";
+import { useCreateCouponMutation } from "@/redux/features/coupon/couponApi";
 import { ErrorResponse } from "@/types";
 import { CouponType } from "@/types/Coupon";
 import { SerializedError } from "@reduxjs/toolkit";
@@ -19,12 +19,12 @@ import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 
 type CouponFormValues = {
-    description: string | null;
-    discount: number;
     code: string;
+    discount: number;
+    description: string;
     type: CouponType;
-    startDate: Date | null;
-    endDate: Date | null;
+    startDate: string;
+    endDate: string;
     isActive: boolean;
 };
 
@@ -36,53 +36,50 @@ export default function CouponForm() {
             description: "",
             type: CouponType.FIXED_AMOUNT,
             isActive: true,
-            startDate: new Date(),
-            endDate: new Date(),
+            startDate: new Date().toISOString().split("T")[0],
+            endDate: new Date().toISOString().split("T")[0],
         },
     });
 
-    const {reset } = form;
+    const { reset } = form;
 
-    const [addBrand, { isSuccess, isLoading, isError, error }] =
-        useCreateBrandMutation();
+    const [createCoupon, { isSuccess, isLoading, isError, error }] =
+        useCreateCouponMutation();
 
     useEffect(() => {
         if (isError) {
             const errorResponse = error as ErrorResponse | SerializedError;
-
             const errorMessage =
                 (errorResponse as ErrorResponse)?.data?.message ||
                 "Something Went Wrong";
 
             toast.error(errorMessage);
         } else if (isSuccess) {
-            toast.success("Brand Successfully Added");
+            toast.success("Coupon Successfully Created");
             reset();
         }
     }, [isError, isSuccess, error, reset]);
 
     const onSubmit = async (data: CouponFormValues) => {
-
-        const loadingToast = toast.loading("Coupon is Creating...");
-        await addBrand(data);
+        const loadingToast = toast.loading("Creating Coupon...");
+        await createCoupon(data);
         toast.dismiss(loadingToast);
     };
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-                {/* Basic Information */}
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
                 <section className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <FormField
                         control={form.control}
-                        name="name"
+                        name="code"
                         render={({ field }) => (
                             <FormItem>
-                                <FormLabel htmlFor="name">Brand Name</FormLabel>
+                                <FormLabel htmlFor="code">Coupon Code</FormLabel>
                                 <FormControl>
                                     <Input
-                                        id="name"
-                                        placeholder="Enter Brand name"
+                                        id="code"
+                                        placeholder="Enter Coupon Code"
                                         {...field}
                                         required
                                     />
@@ -93,14 +90,17 @@ export default function CouponForm() {
                     />
                     <FormField
                         control={form.control}
-                        name="slug"
+                        name="discount"
                         render={({ field }) => (
-                            <FormItem className="col-span-1">
-                                <FormLabel htmlFor="slug">Slug</FormLabel>
+                            <FormItem>
+                                <FormLabel htmlFor="discount">
+                                    Discount Value
+                                </FormLabel>
                                 <FormControl>
                                     <Input
-                                        id="slug"
-                                        placeholder="Enter Brand slug"
+                                        id="discount"
+                                        type="number"
+                                        placeholder="Enter Discount Value"
                                         {...field}
                                         required
                                     />
@@ -111,17 +111,80 @@ export default function CouponForm() {
                     />
                     <FormField
                         control={form.control}
-                        name="image"
+                        name="description"
                         render={({ field }) => (
                             <FormItem className="md:col-span-2">
-                                <FormLabel htmlFor="image">Image</FormLabel>
+                                <FormLabel htmlFor="description">
+                                    Description
+                                </FormLabel>
                                 <FormControl>
                                     <Input
-                                        id="image"
-                                        type="file"
-                                        onChange={(e) =>
-                                            field.onChange(e.target.files?.[0])
-                                        }
+                                        id="description"
+                                        placeholder="Enter Description (Optional)"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="type"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel htmlFor="type">Coupon Type</FormLabel>
+                                <FormControl>
+                                    <select
+                                        id="type"
+                                        className="border rounded-md p-2 w-full"
+                                        {...field}
+                                        required
+                                    >
+                                        <option value={CouponType.FIXED_AMOUNT}>
+                                            Fixed Amount
+                                        </option>
+                                        <option value={CouponType.PERCENTAGE}>
+                                            Percentage
+                                        </option>
+                                    </select>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="startDate"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel htmlFor="startDate">
+                                    Start Date
+                                </FormLabel>
+                                <FormControl>
+                                    <Input
+                                        id="startDate"
+                                        type="date"
+                                        {...field}
+                                        required
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="endDate"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel htmlFor="endDate">End Date</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        id="endDate"
+                                        type="date"
+                                        {...field}
+                                        required
                                     />
                                 </FormControl>
                                 <FormMessage />
@@ -130,10 +193,9 @@ export default function CouponForm() {
                     />
                 </section>
 
-                {/* Submit Button */}
-                <div className="col-span-2 py-5">
-                    <Button type="submit">
-                        {isLoading ? "Creating Brand" : "Create Brand"}
+                <div className="pt-5">
+                    <Button type="submit" disabled={isLoading}>
+                        {isLoading ? "Creating Coupon..." : "Create Coupon"}
                     </Button>
                 </div>
             </form>
